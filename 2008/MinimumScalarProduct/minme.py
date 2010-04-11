4f28360d177935030d77289bc9d31a8a89e4f087
Lukasz Dobrzanski <lukasz.m.dobrzanski@gmail.com>
2010-04-12 00:05:49 +0200

    Updated docstrings for functions of minme.py.

--- 2008/MinimumScalarProduct/minme.py
+++ 2008/MinimumScalarProduct/minme.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python 
 
 from optparse import OptionParser
-import logging
     
 def min_scalar_product(v1, v2, n=None):
+    """Calculates the minimum scalar product for given vectors v1,v2."""
     v1.sort()
     v2.sort()
     if n is None:
@@ -14,7 +14,8 @@ def min_scalar_product(v1, v2, n=None):
     return min_scalar_product    
     
 def min_single_product(v1, v2):
-    """Assumes v1, v2 are allready sorted."""    
+    """Assumes v1, v2 are allready sorted. It does in-place podivication
+    of v1, v2 to remove used coorsinates."""    
     has_min_value, has_max_value = v1, v2 
     if has_min_value[0] * has_max_value[-1] > has_min_value[-1] * has_max_value[0]:
         has_min_value, has_max_value = v2, v1
@@ -23,7 +24,6 @@ def min_single_product(v1, v2):
     has_min_value[:] = has_min_value[1:]  # mofify (v1 or v2) also 
     has_max_value[:] = has_max_value[:-1] # mofify (v1 or v2) also
     return min_product  
-
 
 def main():
     """Main function code. Implementing:
@@ -44,8 +44,11 @@ def main():
         print "Case #%i: %i" % (i+1, result)   
     
 def gen_next_case(filename):
-    """Wrap the imput file format and return engines, phrases 
-    pairs. It uses generator to return them one by one."""
+    """Wrap the imput file format and return a list of
+       - v1 as list of integers (first vector)
+       - v2 as list of integers (second vector)
+       - n as vector size (number of coordinates)
+    It uses generator to return them one by one."""
     next_line = lambda f: f.readline().rstrip()
     with open(filename, "r") as f:
         num_of_cases = int(next_line(f))
