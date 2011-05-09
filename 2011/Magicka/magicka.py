0a2d75f498c05273a6ccf347e7c817a1806a3552
Lukasz Dobrzanski <lukasz.m.dobrzanski@gmail.com>
2011-05-09 18:18:26 +0200

    Simplified. Recursive calls are not needed as magicka combination CAN'T produce "base elements". Made the helper structures for "combined" and "opposing" elements contain bidirectional keys.

--- 2011/Magicka/magicka.py
+++ 2011/Magicka/magicka.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""Code for #gcj 2011 - Magicka""" 
+"""Code for #gcj 2011 - Magicka"""
 
-from __future__ import with_statement
 from optparse import OptionParser
 
 def play_magicka(c_num, d_num, l_num, c, d, l):
-  for i in range(1, len(l)):    
+  l_out = ""
+  for i in range(l_num):
+    l_out += l[i]
     if c_num > 0:
-      for k in (l[i-1], l[i]), (l[i], l[i-1]):
-        if c.has_key(k):
-          l = l[:i-1] + c.get(k) + l[i+1:]
-          return play_magicka(c_num, d_num, l_num, c, d, l) 
+      k = l_out[-2:]
+      if c.has_key(k):
+        l_out = l_out[:-2] + c.get(k)
+        continue
     if d_num > 0:
-      for n, xchar in enumerate(l[:i]):
-        for k in (xchar, l[i]), (l[i], xchar):
-          if d.has_key(k):
-            l = l[i+1:]                                     
-            return play_magicka(c_num, d_num, l_num, c, d, l) 
-  return l
-    
-  
+      for n in range(len(l_out)):
+        k = "".join([l_out[n], l_out[-1]])
+        if d.has_key(k):
+          l_out = ""
+          break
+  return l_out
+
 def solve_case(*items):
   c_num = int(items[0])
   d_num = int(items[c_num+1])
   l_num = int(items[c_num+1+d_num+1])
-  c = dict(((items[i+1][0], items[i+1][1]), items[i+1][2]) for i in range(c_num))
-  d = dict(((items[c_num+1+i+1][0], items[c_num+1+i+1][1]), 1) for i in range(d_num))
+  c, d = dict(), dict()
+  for k, v in [(items[i+1][:2], items[i+1][2]) for i in range(c_num)]:
+    c[k] = c[k[::-1]] = v
+  for k in [items[c_num+1+i+1] for i in range(d_num)]:
+    d[k] = d[k[::-1]] = 1
   l = ""
   if l_num > 0:
     l = items[c_num+1+d_num+1+1]
@@ -40,10 +43,10 @@ def main():
   2. iterating and transorming input data (results stored in table)
   3. printing out results in required format
   """
-  parser = OptionParser(usage="usage: %prog [options] arg") 
-  parser.add_option("-i", "--in-file", dest="in_filename", 
+  parser = OptionParser(usage="usage: %prog [options] arg")
+  parser.add_option("-i", "--in-file", dest="in_filename",
                     help="provide input data by specifing filename")
-  (options, args) = parser.parse_args() 
+  (options, args) = parser.parse_args()
   if options.in_filename is None:
     parser.print_usage()
     exit(0)
@@ -52,7 +55,7 @@ def main():
     result = solve_case(*items)
     results.append(result)
   for i, result in enumerate(results):
-    out = "[" + ', '.join(result) + "]"    
+    out = "[" + ', '.join(result) + "]"
     print "Case #%i: %s" % (i+1, out)
 
 def gen_next_case(filename):
@@ -65,7 +68,7 @@ def gen_next_case(filename):
     while next_case:
       items = next_case.split(' ')
       next_case = next_line(f)
-      yield items 
+      yield items
 
 if __name__ == '__main__':
   main()
